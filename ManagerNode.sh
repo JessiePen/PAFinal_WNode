e995eee46b47076618e2f51f2b2ba5e1693bec6e
yuhe peng <y.peng1998@gmail.com>
2021-12-08 14:31:51 -0500

    modify set up script

--- ManagerNode.sh
+++ ManagerNode.sh
@@ -1,4 +1,5 @@
 sudo apt update
 sudo apt install openjdk-8-jdk
-sudo wget https://github.com/minghuiyang1998/PAFinal_MNode/blob/install/target/PAFinal_MNode-0.0.1-SNAPSHOT.jar
+rm PAFinal_MNode-0.0.1-SNAPSHOT.jar
+sudo wget https://raw.githubusercontent.com/JessiePen/PAFinal_WNode/yuhe/PAFinal_MNode-0.0.1-SNAPSHOT.jar
 sudo java PAFinal_MNode-0.0.1-SNAPSHOT.jar
