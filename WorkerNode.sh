b171469449d2976d1b6d525f6c5a926bb020cffa
yuhe peng <y.peng1998@gmail.com>
2021-12-08 13:21:20 -0500

    modify set up script

--- WorkerNode.sh
+++ WorkerNode.sh
@@ -3,4 +3,4 @@ sudo apt install default-jre
 sudo apt install default-jdk
 sudo wget --no-check-certificate --no-cache --no-cookies https://raw.githubusercontent.com/JessiePen/PAFinal_WNode/yuhe/src/Worker.java
 sudo javac Woker.java
-sudo java Worker
+sudo java Worker.java
